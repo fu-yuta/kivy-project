0501ea1a122f2443dd3d73810c3712c2fe09aaa0
fu-yuta <y.fukusako.046110@gmail.com>
2020-09-04 01:25:54 +0900

    盤面に駒を置けるようにした

--- Othello/othello.py
+++ Othello/othello.py
@@ -1,9 +1,50 @@
 # -*- coding: utf-8 -*-
 from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.graphics import Color, Ellipse, Rectangle
+
+class OthelloGrid(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.num = 8
+        self.tile = [[' ' for x in range(self.num)] for x in range(self.num)]
+        self.turn = 'W'
+
+        self.window_size = Window.width if Window.width <= Window.height else Window.height
+        self.grid = GridLayout(cols=self.num, spacing=[3,3], size=(self.window_size, self.window_size))
+
+        for x in range(self.num):
+            for y in range(self.num):
+                if x == 3 and y == 3 or x == 4 and y == 4:
+                    self.grid.add_widget(WhiteStone())
+                    self.tile[x][y] = 'W'
+                elif x == 4 and y == 3 or x == 3 and y == 4:
+                    self.grid.add_widget(BlackStone())
+                    self.tile[x][y] = 'B'
+                else:
+                    self.grid.add_widget(PutButton(background_color=(0.451,0.3059,0.1882,1), background_normal='', tile_id=[x, y]))
+        self.add_widget(self.grid)
+    
+    def put_stone(self, *args):
+        self.clear_widgets()
+        self.grid = GridLayout(cols=self.num, spacing=[3,3], size=(self.window_size, self.window_size))
+        
+        for x in range(self.num):
+            for y in range(self.num):
+                if self.tile[x][y] == 'W':
+                    self.grid.add_widget(WhiteStone())
+                elif self.tile[x][y] == 'B':
+                    self.grid.add_widget(BlackStone())
+                else:
+                    self.grid.add_widget(PutButton(background_color=(0.451,0.3059,0.1882,1), background_normal='', tile_id=[x, y]))
+        self.add_widget(self.grid)
+
+        self.turn = 'W' if self.turn == 'B' else 'B'
+
 
 class WhiteStone(Label):
     def __init__(self, **kwargs):
@@ -31,19 +72,22 @@ class BlackStone(Label):
         self.canvas.add(Color(0,0,0,1))
         self.canvas.add(Ellipse(pos=self.pos, size=self.size))
 
+class PutButton(Button):
+    def __init__(self, tile_id,  **kwargs):
+        super().__init__(**kwargs)
+        self.tile_id =tile_id
+
+    def on_press(self):
+        print(self.tile_id)
+        x = self.tile_id[0]
+        y = self.tile_id[1]
+        self.parent.parent.tile[x][y] = self.parent.parent.turn
+        self.parent.parent.put_stone()
+
 class OthelloApp(App):
     title = 'オセロ'
     def build(self):
-        num = 8
-        grid = GridLayout(cols=num, spacing=[3,3])
-        for i in range(1,num*num+1):
-            if i == 28 or i == 37:
-                grid.add_widget(WhiteStone())
-            elif i == 29 or i == 36:
-                grid.add_widget(BlackStone())
-            else:
-                grid.add_widget(Button(background_color=(0.451,0.3059,0.1882,1), background_normal=""))
-        return grid
+        return OthelloGrid()
 
 
 OthelloApp().run()
